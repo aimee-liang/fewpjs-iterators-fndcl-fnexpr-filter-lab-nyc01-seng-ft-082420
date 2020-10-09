365a294232adf3706e5d76ba7115ade139c035ab
aimee-liang <aimeeeliang@gmail.com>
2020-10-08 22:18:32 -0400

    Last deliverable left

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// findMatching
+const findMatching = (drivers, string) => {
+  return drivers.filter(name => 
+    name.toUpperCase() === string.toUpperCase()
+  )
+}
+
+// fuzzyMatch
+const fuzzyMatch = (drivers, string) => {
+  return drivers.filter(name => 
+    
+  )
+}
+
+// matchName
+const matchName = (drivers, string) => {
+  return drivers.filter(result => result.name === string)
+}
